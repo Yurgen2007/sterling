ed545a202e481e0232e8a85a9f9b389c5046b7d6
Yurgen2007 <yurgen_perdomo@soy.sena.edu.co>
2024-06-25 15:55:13 -0500

    cargo modificaciones en el ejercicio

--- cajero/cajero.php
+++ cajero/cajero.php
@@ -6,54 +6,50 @@ while ($ingrese){
     print ("opcion 2 , consignar dinero"."\n");
     print ("opcion 3, retirar dinero"."\n");
     print ("opcion 4, salir"."\n");
-    break;
-}
+   
+
     $op=(readline("ingrese una opcion : "));
     if ($op==1){
         function opcion1($ingrese){
             $si=$ingrese;
-            echo "su saldo total es de : $si"; 
+            echo "su saldo total es de : $si". "\n"; 
             return $si;
     }
     opcion1($ingrese);
+
     }
 
     if ($op==2){
-        function opcion2(){
+        function opcion2($ingrese){
             $consig=intval(readline("ingrese el monto que desea consignar :"));
-            echo "consignacion exitosa....";
+            echo "consignacion exitosa...."."\n";
+            $ultimo=$ingrese;
+            $operacion2=$ultimo+$consig;
+            echo "el monto total en su cuenta es de : $operacion2". "\n";
+            return $operacion2;
             return $consig;
         
     }
-    opcion2();
+    opcion2($ingrese);
     }
-
 
     if ($op==3){
-        function opcion3(){
+        function opcion3($ingrese){
             $retirar=intval(readline("¿cuanto dinero desea retirar?"));
-            echo "retiro exitoso.....";
+            echo "retiro exitoso....."."\n";
+            $no=$ingrese;
+            $operacion=$no-$retirar;
+            echo "el monto total en su cuenta es de : $operacion". "\n";
+            return $operacion;
             return $retirar;
     }
-    opcion3();
+    opcion3($ingrese);
     }
 
 
     if ($op==4){
         echo "saliendo......";
     }
-    
-
-
-
-   
-
-    
-
-
-
-
-
-
+}
 
 ?>
